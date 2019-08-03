dba1abea065d9d7bdfd76b60ea86c0409f250790
Ismael Sane <six.echo.sa@gmail.com>
2019-08-03 13:47:08 +0200

    Initialize repository

---
+++ lib/isane_palindrome.rb
@@ -0,0 +1,6 @@
+require "isane_palindrome/version"
+
+module IsanePalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/isane_palindrome/version.rb
@@ -0,0 +1,3 @@
+module IsanePalindrome
+  VERSION = "0.1.0"
+end
